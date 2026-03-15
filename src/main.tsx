26708485df9593cf94566e8083be944b9adad599
afifalhauzan <afifalhauzan@student.ub.ac.id>
2026-03-15 22:36:27 +0700

    feat: add @vercel/analytics dependency and integrate Analytics component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { theme } from './theme/theme';
 import GlobalStyles from "@mui/material/GlobalStyles";
 import App from './App';
+import { Analytics } from "@vercel/analytics/next"
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
@@ -19,6 +20,7 @@ if (container) {
   const root = createRoot(container);
   root.render(
     <React.StrictMode>
+      <Analytics />
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <GlobalStyles
